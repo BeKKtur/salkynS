70fc562958128efeee1d4a63a4311e78a78f74ef
Bektur Batyrbekov <batyrbekovbektur0@gmail.com>
2026-05-24 22:52:59 +0600

    feat: prepare backend for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,10 +6,9 @@ import createApi from "./createApi";
 
 const app = createApi();
 // Забираем порт из .env, а если его там нет — оставляем дефолтный 5555
-const port = process.env.PORT || 5555;
+// Превращаем PORT в число с помощью Number()
+const PORT = Number(process.env.PORT) || 5555;
 
-console.log("1. Скрипт бэкенда успешно стартовал...");
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
